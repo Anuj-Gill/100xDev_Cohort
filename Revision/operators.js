147fb0c17e6b6498658000e36475301f3668cfca
Anuj Gill <anujgill212@gmail.com>
2023-11-25 07:41:56 +0530

    Revised operators

---
+++ Revision/operators.js
@@ -0,0 +1,77 @@
+// Question-1
+// very simple
+
+
+
+// Question-2
+let z = 2;
+
+
+
+// Question-3
+let a,b;
+a = 7;
+b = 18
+if (a > b)
+  console.log("a is greater")
+else if (a === b)
+  console.log('both are equal')
+else
+  console.log('b is greater than a')
+
+
+
+// Question-4
+
+let firstName = "Anuj"
+let lastName = "Gill"
+
+if(firstName === "Anuj" && lastName === "Gill")
+  console.log("It's you Gill")
+else if(firstName === "Anuj" || lastName === "Gill")
+  console.log("It may be you Gill")
+else 
+  console.log("It's not you Gill")
+
+
+// Question-5
+// Bitwise operations
+
+console.log(3 & 6);
+console.log(3 | 6);
+console.log(3 ^ 6);
+console.log(6 << 2);
+console.log(6 >> 2);
+
+
+// Question-6
+let g,h;
+g = 7;
+h = 18;
+console.log(g > h ? 'a is greater' : 'b is greater')
+
+
+
+// QUestion-7
+
+console.log(typeof 4)
+console.log(typeof "abc")
+console.log(typeof null)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof -12324325442113242)
+
+
+//Question-8
+console.log("abcd" + "efgh")
+let random = "india"
+let newName = "Bharat"
+console.log(newName + ' not ' + random)
+
+// Question-9
+//similar to question-7, just assign values to some variables and then use typeof
+
+// Question-10
+let obj = {a:1,b:2}
+console.log('a' in obj)
+console.log('d' in obj)
