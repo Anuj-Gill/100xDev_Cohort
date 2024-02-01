746a6eea428a30574c6f1f5798f0847c60da0f5f
Anuj Gill <anujgill212@gmail.com>
2024-02-01 06:32:31 +0530

    Completed Navbar

--- Classes/tailwind-learn/src/App.jsx
+++ Classes/tailwind-learn/src/App.jsx
@@ -8,14 +8,17 @@ function App() {
 
   return (
     <>
-      <body className='pt-20 mb-100'></body>
+      <body className='pt-20 mb-100 bg-gray-50'>
       <Navbar />
-      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5'>
       <RevenueCard title={"Next Payout"} amount={"92,000.00"} orderCount={13} first={true}/>
       <RevenueCard title={"Amount Pending"} amount={"92,000.00"} orderCount={13}/>
       <RevenueCard title={"Amount Processed"} amount={"92,000.00"} />
 
       </div>
+
+        
+      </body>
     </>
   )
 }
